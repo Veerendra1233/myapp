97845ec4b881415ccc5cf5e1a2851ab2c43f4f7a
Veerendra1233 <veerendra2koncha@gmail.com>
2026-02-05 19:46:00 +0530

    Initialize Go service with HTTP server

---
+++ go-service/main.go
@@ -0,0 +1,13 @@
+package main
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Welcome to the Go Service! Build: ${GITHUB_SHA}")
+    })
+    fmt.Println("Server starting on port 8080...")
+    http.ListenAndServe(":8080", nil)
+}
